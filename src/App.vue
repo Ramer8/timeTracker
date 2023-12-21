7ae7f936a4f34764531e5a8d57bde5f656e07a9a
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2023-12-21 17:01:13 +0100

    first commit first structure

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  
+    <div class="p-6 bg-color: bg-gray-200 text-white  h-screen">
+      <ShowData  v-if="info" :info="info"></ShowData>
+      <button class="bg-gray-500 hover:bg-gray-600 px-2 py-1 m-2 rounded-full " 
+      @click="getData">Get Data </button>
+    </div>
+  
+</template>
+<script>
+ import ShowData from './components/ShowData.vue';
+ import axios from 'axios';
+
+export default {
+  components: {
+    ShowData
+  },
+  data() {
+    return {
+      info: '',
+      newData: ''
+    };
+  },
+  methods: {
+    getData() {
+        axios
+          .get('http://localhost:3000/data')
+          .then(response => (this.info = response))
+          console.log(this.info.data);
+          
+    }
+  }
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/ShowData.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class=" flex gap-2  bg-gray-300 text-gray-900 font-bold rounded-full lg:w-1/2 sm:w-fit text-center p-4">
+     <div >
+       {{ info.data.workEntryIn.date.slice(11, -6) }} 
+     </div>
+      <button class=" bg-green-600 rounded-full  font-medium w-32 text-white px-2 py-1">Entar</button>
+     | 
+     <img class='h-10 mx-auto object-cover rounded-full w-10'
+     width='50' height="30" src="/Users/ramirop/Documents/js/timeTracker/public/profilemanpic.jpeg" >
+       <div  class="flex w-1/3 bg-gray-300 ">
+         {{ info.data.employee.firstName }}
+         {{ info.data.employee.lastName }}
+       </div>
+ </div>
+ 
+</template>
+<script>
+export default {
+  data() {
+    return {
+      newDate: '',
+    };
+  },
+props: {
+  info: Object,
+  },
+  methods:{
+    splitDate(date) {
+     newDate =  date.slice(11, -6)
+console.log(newDate);
+    }
+  }
+}
+</script>
+<style></style>
